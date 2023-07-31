b59dd46da818520cfb80f72b2ac3dec189c3db8d
nani8ot <git@wiedemer.xyz>
2023-07-31 16:01:59 +0200

    add: xdg-desktop-portal-termfilechooser to use terminal file manager like ranger

---
+++ pkgs/development/libraries/xdg-desktop-portal-termfilechooser/default.nix
@@ -0,0 +1,45 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, makeWrapper
+, meson
+, ninja
+, pkg-config
+, wayland-protocols
+, wayland-scanner
+, inih
+, libdrm
+, mesa
+, scdoc
+, systemd
+, wayland
+}:
+
+stdenv.mkDerivation rec {
+  pname = "xdg-desktop-portal-termfilechooser";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "GermainZ";
+    repo = pname;
+    rev = "main";
+    sha256 = "sha256-645hoLhQNncqfLKcYCgWLbSrTRUNELh6EAdgUVq3ypM=";
+  };
+
+  strictDeps = true;
+  depsBuildBuild = [ pkg-config ];
+  nativeBuildInputs = [ meson ninja pkg-config scdoc wayland-scanner makeWrapper ];
+  buildInputs = [ inih libdrm mesa systemd wayland wayland-protocols ];
+
+  mesonFlags = [
+    "-Dsd-bus-provider=libsystemd"
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/GermainZ/xdg-desktop-portal-termfilechooser";
+    description = "xdg-desktop-portal backend for wlroots and the likes of ranger";
+    maintainers = with maintainers; [ nani8ot ];
+    platforms = platforms.linux;
+    license = licenses.mit;
+  };
+}
