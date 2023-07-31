fc4a8d4d7e418ef7e31f253725e141afe261cb0e
nani8ot <github@wiedemer.xyz>
2023-07-31 23:49:35 +0200

    Update hash

--- pkgs/development/libraries/xdg-desktop-portal-termfilechooser/default.nix
+++ pkgs/development/libraries/xdg-desktop-portal-termfilechooser/default.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
     owner = "nani8ot";
     repo = pname;
     rev = "main";
-    sha256 = "sha256-4s3n5EONwmDhdMrs7NdCqM/IAcNXmmbuhv84btCred1=";
+    sha256 = "sha256-OzJyFyn2RMiKrE20SnGBCw7EP8FkTSuva8yWxlk2DTk=";
   };
 
   strictDeps = true;
