c6a2f98336cd7d24c9f4a6cfa8189dd468fc8759
nani8ot <git@wiedemer.xyz>
2024-09-05 18:14:08 +0200

    prometheus-qbittorrent-exporter: Init at 1.2.6

---
+++ pkgs/servers/monitoring/qbittorrent-exporter.nix
@@ -0,0 +1,28 @@
+{ lib, stdenv, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "prometheus-qbittorrent-exporter";
+  version = "1.2.6";
+
+  src = fetchFromGitHub {
+    owner = "martabal";
+    repo = "qbittorrent-exporter";
+    rev = "v${version}";
+    hash = "sha256-WXId+9CKjcMmBkgSFEalUqa9m/04fOuprIpGF1wUuTs=";
+  } + "/src";
+
+  vendorHash = "sha256-G+r3V6oDtr57pmk2Rrudbk9cnf0wcldJ2TnYEEzlG2o=";
+
+  nativeCheckInputs = [ ];
+
+  meta = with lib; {
+    homepage = "https://github.com/martabal/qbittorrent-exporter";
+    description = "Prometheus exporter for qBittorrent";
+    changelog =
+      "https://github.com/martabal/qbittorrent-exporter/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ nani8ot ];
+    platforms = platforms.unix;
+  };
+}
+
